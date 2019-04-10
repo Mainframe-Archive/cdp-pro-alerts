052ab74bca8f8e7a639177f5744ee35ee96331d4
Miloš Mošić <milos@mainframe.com>
2019-04-10 15:04:29 +0100

    Refactor email module to reuse the common functions

--- lib/cdp_pro/email.ex
+++ lib/cdp_pro/email.ex
@@ -1,24 +1,28 @@
 defmodule CdpPro.Email do
   use Bamboo.Phoenix, view: CdpProWeb.EmailView
-  import Bamboo.Email
 
   @from_email Application.get_env(:cdp_pro, :subscriptions_email)
 
   def confirm_subscription_email(subscription) do
-    new_email()
-    |> from(@from_email)
-    |> to(subscription.email)
-    |> put_layout({CdpProWeb.LayoutView, :email})
+    subscription
+    |> base_email()
     |> subject("Confirm your subscription to CDP PRO alerts")
     |> render(:text_and_html_email)
   end
 
   def warning_email(subscription) do
+    subscription
+    |> base_email()
+    |> subject("CDP PRO alert: Your collateralization ratio has reached the CDP Guard limit")
+    |> render(:warning)
+  end
+
+  defp base_email(subscription) do
     new_email()
     |> from(@from_email)
     |> to(subscription.email)
+    |> assign(:subscription, subscription)
+    |> put_header("Reply-To", @from_email)
     |> put_layout({CdpProWeb.LayoutView, :email})
-    |> text_body("Welcome")
-    |> subject("CDP PRO alert: Your collateralization ratio has reached the CDP Guard limit")
   end
 end
