d7772ce141877e207a0b0ff9377d1421c8f9b870
Miloš Mošić <milos@mainframe.com>
2019-04-16 14:40:45 +0100

    Use a variable for the title in email and pages

--- config/config.exs
+++ config/config.exs
@@ -10,6 +10,7 @@ use Mix.Config
 config :cdp_pro,
   ecto_repos: [CdpPro.Repo],
   generators: [binary_id: true],
+  title: "CDP PRO Alerts",
   logo_url: System.get_env("LOGO_URL") || "https://s3.amazonaws.com/cdp-pro/cdp-pro-logo.png",
   subscriptions_email: System.get_env("SUBSCRIPTION_EMAIL") || "cdp-pro-alerts@mainframeos.com",
   polling_interval: (System.get_env("POLLING_INTERVAL") || "15") |> String.to_integer()
